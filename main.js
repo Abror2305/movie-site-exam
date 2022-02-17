d06ebc099db1df766aa8a6750a909c346cc5c190
Abror2305 <abroralisherov90@gmail.com>
2022-02-17 10:34:52 +0500

    Project started

---
+++ main.js
@@ -0,0 +1,7 @@
+// let API_KEY = 'b971c2f0de8767f08d2bb84160ba24b7'
+
+let API_KEY = 'dcea1fd7b3e65d34387ad6de7ef9cc5e'
+
+let tokenTop = `https://api.themoviedb.org/3/movie/top_rated?api_key=${API_KEY}&page=1` 
+let tokenPopular = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&page=1`
+let tokenUpComing = `https://api.themoviedb.org/3/movie/upcoming?api_key=${API_KEY}&page=1`
