038d46cc9a55c46185a31a178658b16e082b6047
Abror2305 <abroralisherov90@gmail.com>
2022-02-17 12:55:04 +0500

    Pagination competed

--- main.js
+++ main.js
@@ -1,15 +1,22 @@
 // let API_KEY = 'b971c2f0de8767f08d2bb84160ba24b7'
+
+let page = 1
+
 // APIs
 let API_KEY = 'dcea1fd7b3e65d34387ad6de7ef9cc5e'
-let tokenTop = `https://api.themoviedb.org/3/movie/top_rated?api_key=${API_KEY}&page=1` 
-let tokenPopular = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&page=1`
-let tokenUpComing = `https://api.themoviedb.org/3/movie/upcoming?api_key=${API_KEY}&page=1`
+let tokenTop = `https://api.themoviedb.org/3/movie/top_rated?api_key=${API_KEY}&page=`
+let tokenPopular = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&page=`
+let tokenUpComing = `https://api.themoviedb.org/3/movie/upcoming?api_key=${API_KEY}&page=`
+let defaultActive = window.localStorage.active || 'top'
 
 //DOM elements
 const movieList = document.querySelector('.append')
 const [topMovie,popular,upcoming] = document.querySelectorAll('.btns')
-console.log(topMovie);
+const prev = document.querySelector('.prev')
+const next = document.querySelector('.next')
+const pagNum = document.querySelector('.title')
 
+// render movie
 function renderMovie(data) {
     movieList.innerHTML = ''
     for (const el of data) {
@@ -34,22 +41,72 @@ function renderMovie(data) {
     
 }
 
-upcoming.onclick = async () => {
-    const response = await fetch(tokenUpComing)
-    const data = await response.json()
-    renderMovie(data.results)
+// active buttons
+topMovie.onclick = async () => {
+    page = 1
+    pagNum.innerText = page
+    let response =await getData(tokenTop+page)
+    renderMovie(response)
+    window.localStorage.active = 'top'
 }
 popular.onclick = async () => {
-    const response = await fetch(tokenPopular)
-    const data = await response.json()
-    renderMovie(data.results)
+    page = 1
+    pagNum.innerText = page
+    let response =await getData(tokenPopular+page)
+    renderMovie(response)
+    window.localStorage.active = 'popular'
 }
-topMovie.onclick = async () => {
-    const response = await fetch(tokenTop)
-    const data = await response.json()
-    renderMovie(data.results)
+upcoming.onclick = async () => {
+    page = 1
+    pagNum.innerText = page
+    let response =await getData(tokenUpComing+page)
+    renderMovie(response)
+    window.localStorage.active = 'upcoming'
 }
 
+// pagination
+prev.onclick = async () => {
+    if(page > 1) {
+        let active = window.localStorage.active
+        page--
+        pagNum.innerText = page
+        if (active === 'top') {
+            renderMovie(await getData(tokenTop+page))
+        } else if (active === 'popular') {
+            renderMovie(await getData(tokenPopular+page))
+        }
+        else if (active === 'upcoming') {
+            renderMovie(await getData(tokenUpComing+page))
+        }
+    }
+}
+next.onclick = async () => {
+    let active = window.localStorage.active
+    ++page
+    pagNum.innerText = page
+    if (active === 'top') {
+        renderMovie(await getData(tokenTop+page))
+    } else if (active === 'popular') {
+        renderMovie(await getData(tokenPopular+page))
+    }
+    else if (active === 'upcoming') {
+        renderMovie(await getData(tokenUpComing+page))
+    }
+}
+function filter(){}
 
 
-let a = fetch(tokenTop).then(data => data.json()).then(data => renderMovie(data.results))
+// default active button
+switch (defaultActive) {
+    case 'popular':
+        popular.click()
+        break;
+    case 'top':
+        topMovie.click()
+        break;
+    case 'upcoming':
+        upcoming.click()
+        break;
+    default:
+        topMovie.click()
+}
--- util.js
+++ util.js
@@ -1,3 +1,9 @@
 function createElements(...elements){
     return elements.map(element => document.createElement(element));
 }
+
+async function getData(url) {
+    const response = await fetch(url)
+    const data = await response.json()
+    return data.results
+}
